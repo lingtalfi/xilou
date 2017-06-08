2439a868971a4a8729d2ae27a514775701a16b6b
lingtalfi <lingtalfi@gmail.com>
2017-06-08 12:14:08 +0200

    update

---
+++ public/crud/list/zilu.article.php
@@ -0,0 +1,72 @@
+<?php
+
+use Crud\CrudHelper;
+use Crud\CrudModule;
+
+$fields = '
+id,
+reference_lf,
+reference_hldp,
+descr_fr,
+descr_en,
+ean,
+photo,
+logo,
+long_desc_en
+';
+
+
+$query = "select
+%s
+from zilu.article
+";
+
+
+$table = CrudModule::getDataTable("zilu.article", $query, $fields, ['id']);
+
+$table->title = "Article";
+
+
+$table->columnLabels= [
+    "id" => "id",
+    "reference_lf" => "reference lf",
+    "reference_hldp" => "reference hldp",
+    "descr_fr" => "descr fr",
+    "descr_en" => "descr en",
+    "ean" => "ean",
+    "photo" => "photo",
+    "logo" => "logo",
+    "long_desc_en" => "long desc en",
+];
+
+
+$table->hiddenColumns = [
+    "id",
+];
+
+
+$n = 30;
+$table->setTransformer('descr_fr', function ($v) use ($n) {
+    return substr($v, 0, $n) . '...';
+});
+$table->setTransformer('descr_en', function ($v) use ($n) {
+    return substr($v, 0, $n) . '...';
+});
+$table->setTransformer('long_desc_en', function ($v) use ($n) {
+    return substr($v, 0, $n) . '...';
+});
+$table->setTransformer('photo', function ($v) use ($n) {
+	if("/" === $v){
+		return "";
+	}
+    return '<img width="150" src="'. $v .'">';
+});
+$table->setTransformer('logo', function ($v) use ($n) {
+	if("/" === $v){
+		return "";
+	}
+    return '<img width="150" src="'. $v .'">';
+});
+
+
+$table->displayTable();
